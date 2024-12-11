2cd6fecaa20e3ee19d08c97167f10c6397bf1858
dichalov <dmitrychaloff@gmail.com>
2024-12-11 18:50:09 +0300

    Add files via upload

---
+++ modul_1_9.py
@@ -0,0 +1,7 @@
+#Практическое задание по уроку "Строки и индексация строк"
+example="Топинамбур"
+print(example[0])#Т
+print(example[-1])#р
+print(example[5:])#амбур
+print(example[::-1])#рубманипоТ
+print(example[1::2])#оиабр
