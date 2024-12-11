04ec768a45dd20981acdaa8e7377df1b40f00f6e
dichalov <dmitrychaloff@gmail.com>
2024-12-11 18:04:56 +0300

    Add files via upload

---
+++ modul_1_6.py
@@ -0,0 +1,6 @@
+#Практическое задание по теме "Переменные"
+number_of_homework_completed=12
+number_of_hours_spent=1.5
+name_of_course="Python"
+time_per_task=(number_of_hours_spent/number_of_homework_completed)
+print("Курс:",name_of_course,", всего задач:",number_of_homework_completed,", среднее время выполнения:",time_per_task,".")
