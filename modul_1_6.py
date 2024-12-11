4f3ad1cf967e9cfd0cc99055da4b404a77e6213a
dichalov <dmitrychaloff@gmail.com>
2024-12-11 18:13:13 +0300

    Add files via upload

--- modul_1_6.py
+++ modul_1_6.py
@@ -3,4 +3,4 @@ number_of_homework_completed=12
 number_of_hours_spent=1.5
 name_of_course="Python"
 time_per_task=(number_of_hours_spent/number_of_homework_completed)
-print("Курс:",name_of_course,", всего задач:",number_of_homework_completed,", среднее время выполнения:",time_per_task,".")
+print("Курс:",name_of_course,", всего задач:",number_of_homework_completed,", затрачено часов:",number_of_hours_spent,", среднее время выполнения:",time_per_task,".")
